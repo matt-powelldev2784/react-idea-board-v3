93b30497b639ad77c267bde6202c6a50b3130e61
Matthew Powell <matt.powell2784@gmail.com>
2024-08-29 20:30:18 +0100

    ci: add global styles to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,40 @@
 export default {
   content: ['index.html', './src/**/*.{js,jsx,ts,tsx,vue,html}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryBlue: '#356597',
+        lightBlue: '#337bcc',
+      },
+      screens: {
+        sm: '0px',
+        md: '600px',
+        lg: '1100px',
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      addUtilities({
+        '.flexCol': {
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          flexDirection: 'column',
+          flexWrap: 'wrap',
+          width: '100%',
+          margin: 0,
+        },
+        '.flexRow': {
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          flexDirection: 'row',
+          flexWrap: 'wrap',
+          width: '100%',
+          margin: 0,
+        },
+      });
+    },
+  ],
 };
