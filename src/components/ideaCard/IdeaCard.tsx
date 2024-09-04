9d237bf7fa2c1659c64689db29a8262d17e46972
Matthew Powell <matt.powell2784@gmail.com>
2024-09-04 20:13:09 +0100

    feat: add edit and delete icon to idea card

--- src/components/ideaCard/IdeaCard.tsx
+++ src/components/ideaCard/IdeaCard.tsx
@@ -1,20 +1,39 @@
 import kidIdeaImage from '../../assets/images/kid_idea.png';
+import whiteBinIcon from '../../assets/images/bin_white.png';
+import editIcon from '../../assets/images/edit.png';
 
 export const IdeaCard = () => {
   return (
     <div className="flex w-full max-w-[800px] flex-col items-center overflow-hidden rounded-xl border border-gray-300">
-      <div className="h-full w-full bg-pink-gradient flexRow">
-        <img src={kidIdeaImage} alt="kid_idea" className="h-40" />
+      <div className="relative h-full w-full bg-pink-gradient flexRow">
+        <img
+          src={kidIdeaImage}
+          alt="child with light bulb"
+          className="max-w-[250px] object-contain"
+        />
+        <img
+          src={whiteBinIcon}
+          alt="white bin icon"
+          className="absolute right-4 top-3 h-[27px] w-[20px] cursor-pointer"
+        />
       </div>
-      <div className="p-4">
+
+      <div className="relative p-4">
+        <img
+          src={editIcon}
+          alt="pen icon"
+          className="absolute bottom-3 right-4 h-[35px] w-[35px] cursor-pointer"
+        />
+
         <h1 className="font-bold">Idea Title</h1>
-        <p>
+
+        <p className="">
           Lorem Ipsum is simply dummy text of the printing and typesetting
           industry. Lorem Ipsum has been the industrys standard dummy text ever
           since the 1500s, when an unknown printer took a galley of type and
           scrambled it to make a type specimen book.
         </p>
-        <p className="mt-2 text-xs">Last Updated 24/09/24 14:00</p>
+        <p className="mt-6 text-xs">Last Updated 24/09/24 14:00</p>
       </div>
     </div>
   );
