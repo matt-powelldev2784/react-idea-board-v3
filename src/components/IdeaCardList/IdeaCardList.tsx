6a5361c90c0b88a40ff4308fdfd673de9c8b3c52
Matthew Powell <matt.powell2784@gmail.com>
2024-09-05 19:23:14 +0100

    refactor: change idea list wrapper to section

--- src/components/IdeaCardList/IdeaCardList.tsx
+++ src/components/IdeaCardList/IdeaCardList.tsx
@@ -5,7 +5,7 @@ export const IdeaCardList = () => {
   const { ideaList } = useSortedIdeas({ sortBy: 'date' });
 
   return (
-    <div className="m-4 mb-24 flex flex-col gap-4">
+    <section className="m-4 mb-24 flex flex-col gap-4">
       {ideaList.length ? (
         <h1 className="text-center text-xl font-bold">Idea List</h1>
       ) : null}
@@ -13,6 +13,6 @@ export const IdeaCardList = () => {
       {ideaList.map((idea) => {
         return <IdeaCard {...idea} key={idea.key} />;
       })}
-    </div>
+    </section>
   );
 };
