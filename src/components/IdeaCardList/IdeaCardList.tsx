3b05000c973e3b4779327ec44035633f6ffbceeb
Matthew Powell <matt.powell2784@gmail.com>
2024-09-11 17:49:23 +0100

    feat: add sort buttons

--- src/components/IdeaCardList/IdeaCardList.tsx
+++ src/components/IdeaCardList/IdeaCardList.tsx
@@ -1,11 +1,30 @@
+import { useState } from 'react';
 import { IdeaCard } from '../ideaCard/IdeaCard';
 import { useSortedIdeas } from './hooks/useSortedIdeas';
 
 export const IdeaCardList = () => {
-  const { ideaList } = useSortedIdeas({ sortBy: 'date' });
+  const [sortBy, setSortBy] = useState<'title' | 'date'>('date');
+
+  const { ideaList } = useSortedIdeas({ sortBy });
 
   return (
     <section className="m-4 mb-24 flex flex-col gap-4">
+      <div className="flex justify-center gap-4">
+        <button
+          onClick={() => setSortBy('date')}
+          className="rounded-lg bg-primaryBlue px-4 py-2 text-white"
+        >
+          Sort by Date
+        </button>
+
+        <button
+          onClick={() => setSortBy('title')}
+          className="rounded-lg bg-primaryBlue px-4 py-2 text-white"
+        >
+          Sort by Title
+        </button>
+      </div>
+
       {ideaList.length ? (
         <h1 className="text-center text-xl font-bold">Idea List</h1>
       ) : null}
