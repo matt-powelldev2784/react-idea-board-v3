2e0c2860ced88bee249a8c6c587e6576426ac3c6
Matthew Powell <matt.powell2784@gmail.com>
2024-09-23 19:18:37 +0100

    fix: fix updated recently hook by parsing last updated date

--- src/components/ideaCard/hooks/useIdeaUpdatedRecently.ts
+++ src/components/ideaCard/hooks/useIdeaUpdatedRecently.ts
@@ -1,12 +1,20 @@
 import { useState, useEffect } from 'react';
-import { differenceInSeconds, format } from 'date-fns';
+import { differenceInSeconds, parse } from 'date-fns';
 
 const useIdeaUpdatedRecently = (lastUpdated: string) => {
   const [isUpdatedRecently, setIsUpdatedRecently] = useState(false);
 
   useEffect(() => {
-    const now = format(new Date(), 'dd-MM-yy HH:mm:ss');
-    const secondsDifference = differenceInSeconds(now, lastUpdated);
+    const now = new Date();
+
+    const parsedLastUpdated = parse(
+      lastUpdated,
+      'dd-MM-yy HH:mm:ss',
+      new Date(),
+    );
+
+    const secondsDifference = differenceInSeconds(now, parsedLastUpdated);
+
     // If the idea was updated within the last 60 seconds, updated recently is true
     const updatedRecently = secondsDifference <= 60 ? true : false;
 
